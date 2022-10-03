eaf510da383ce2532b90a361269f7aca31685703
ahs0432 <ahs0432@naver.com>
2022-10-04 00:05:38 +0900

    ✅ 기존 코드 일부 커밋

---
+++ GoLang/11022/11022.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+
+	for i := 1; i <= n; i++ {
+		var n1, n2 int
+		fmt.Scanln(&n1, &n2)
+
+		fmt.Println("Case #"+strconv.Itoa(i)+":", n1, "+", n2, "=", n1+n2)
+	}
+}
