f23b3e8de273ce7cfea9d17f1e64c43d29c10a63
ahs0432 <ahs0432@naver.com>
2022-10-08 00:46:28 +0900

    ✅ 기존 코드 일부 커밋

---
+++ GoLang/25083/25083.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("         ,r'\"7")
+	fmt.Println("r`-_   ,'  ,/")
+	fmt.Println(" \\. \". L_r'")
+	fmt.Println("   `~\\/")
+	fmt.Println("      |")
+	fmt.Print("      |")
+}
