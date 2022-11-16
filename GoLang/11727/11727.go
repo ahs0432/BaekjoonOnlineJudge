011180884108612b5f7532b66f53ef54b1ba807f
ahs0432 <ahs0432@naver.com>
2022-11-17 00:17:40 +0900

    ✅  11727 문제 풀이

---
+++ GoLang/11727/11727.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+
+	if n > 1 {
+		dp[2] = 3
+
+		for i := 3; i <= n; i++ {
+			dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
+		}
+	}
+
+	fmt.Println(dp[n])
+}
