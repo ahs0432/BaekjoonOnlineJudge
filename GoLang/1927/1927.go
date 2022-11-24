db9965affc5cb24cac6a8f4fc3e131662bf57813
ahs0432 <ahs0432@naver.com>
2022-11-25 00:20:19 +0900

    ✅  1927 문제 풀이

---
+++ GoLang/1927/1927.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(element interface{}) {
+	*h = append(*h, element.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	element := old[n-1]
+	*h = old[0 : n-1]
+	return element
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	h := &IntHeap{}
+	heap.Init(h)
+	for i := 0; i < n; i++ {
+		var in int
+		fmt.Fscanln(reader, &in)
+
+		if in == 0 {
+			if h.Len() == 0 {
+				fmt.Fprintln(writer, 0)
+			} else {
+				fmt.Fprintln(writer, heap.Pop(h))
+			}
+		} else {
+			heap.Push(h, in)
+		}
+	}
+	writer.Flush()
+}
