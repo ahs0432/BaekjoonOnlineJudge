aba960bc369e9145602332823ac6e8dda1de5e4b
ahs0432 <ahs0432@naver.com>
2023-01-14 01:06:20 +0900

    ✅ 2445 문제 풀이

---
+++ GoLang/2445/2445.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	for i := 1; i <= n; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+
+		for j := 0; j < (n-i)*2; j++ {
+			fmt.Print(" ")
+		}
+
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	for i := n - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+
+		for j := 0; j < (n-i)*2; j++ {
+			fmt.Print(" ")
+		}
+
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
