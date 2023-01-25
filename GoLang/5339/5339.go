3b933c0e64bb86b1bc8cdda47fe7f6a75579a2b2
ahs0432 <ahs0432@naver.com>
2023-01-26 00:22:53 +0900

    ✅ 5339 문제 풀이

---
+++ GoLang/5339/5339.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |")
+}
