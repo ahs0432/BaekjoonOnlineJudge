b7c48c455ea7432f66c191b7b244a40a888e4d92
ahs0432 <ahs0432@naver.com>
2023-03-07 00:11:36 +0900

    ✅ 2752 문제 풀이

---
+++ GoLang/2752/2752.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	table := make([]int, 3)
+	fmt.Fscanln(reader, &table[0], &table[1], &table[2])
+
+	sort.Slice(table, func(i, j int) bool {
+		return table[i] < table[j]
+	})
+
+	for _, i := range table {
+		fmt.Print(i, " ")
+	}
+	fmt.Println()
+}
