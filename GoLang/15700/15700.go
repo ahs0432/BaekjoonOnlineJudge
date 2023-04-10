34858e2faee03eebc1a11cbd00b1ea7017435d52
ahs0432 <ahs0432@naver.com>
2023-04-11 00:17:52 +0900

    ✅ 15700 문제 풀이

---
+++ GoLang/15700/15700.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var n, m int
+	fmt.Fscanln(reader, &n, &m)
+
+	fmt.Println(n * m / 2)
+}
