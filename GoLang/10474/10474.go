07d247d820e9ace9a02e220afdde43897324bf6f
ahs0432 <ahs0432@naver.com>
2023-11-23 04:13:39 +0900

    ✅ 10474 문제 풀이

---
+++ GoLang/10474/10474.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var a, b int
+	for {
+		fmt.Fscanln(reader, &a, &b)
+		if a == 0 && b == 0 {
+			break
+		}
+		fmt.Fprintln(writer, a/b, a%b, "/", b)
+	}
+	writer.Flush()
+}
