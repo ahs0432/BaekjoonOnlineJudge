d5eb96d0aba9fda90ff8509cc4cf14f946a4075a
ahs0432 <ahs0432@naver.com>
2024-02-17 00:28:48 +0900

    ✅ 23808 문제 풀이

---
+++ GoLang/23808/23808.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	s1 := ""
+	for i := 0; i < n; i++ {
+		s1 += "@"
+	}
+	for i := 0; i < 3*n; i++ {
+		s1 += " "
+	}
+	for i := 0; i < n; i++ {
+		s1 += "@"
+	}
+
+	for i := 0; i < n*2; i++ {
+		fmt.Fprintln(writer, s1)
+	}
+
+	s2 := ""
+	for i := 0; i < n*5; i++ {
+		s2 += "@"
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(writer, s2)
+	}
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(writer, s1)
+	}
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(writer, s2)
+	}
+
+	writer.Flush()
+}
