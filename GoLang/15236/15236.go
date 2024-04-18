925cabe14f04b52561e6ae08b19fc4fddc765d9f
ahs0432 <ahs0432@naver.com>
2024-04-19 00:17:02 +0900

    ✅ 15236 문제 풀이

---
+++ GoLang/15236/15236.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var n int
+	fmt.Fscanln(reader, &n)
+	fmt.Println(n * (n + 1) * (n + 2) / 2)
+}
