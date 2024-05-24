a6709e2ff06828fb5b3cfc5ae04d10b3b9558cde
ahs0432 <ahs0432@naver.com>
2024-05-25 06:45:35 +0900

    ✅ 13073 문제 풀이

---
+++ GoLang/13073/13073.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var t int
+	fmt.Fscanln(reader, &t)
+
+	var tmp int
+	for i := 0; i < t; i++ {
+		fmt.Fscanln(reader, &tmp)
+		fmt.Fprintln(writer, tmp*(tmp+1)/2, tmp*tmp, tmp*(tmp+1))
+	}
+	writer.Flush()
+}
