c06ca312692453d8316bbdf934809d62bab470d9
ahs0432 <ahs0432@naver.com>
2025-08-06 00:15:24 +0900

    ✅ 27959 문제 풀이

---
+++ GoLang/27959/27959.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var n, m int
+	fmt.Fscanln(reader, &n, &m)
+
+	if n*100 >= m {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
